64ec5fb81f398cef95c7f8a1b8831e3f432d04f3
Mehmet Yildiz <mehmet.iu@gmail.com>
2018-12-15 22:46:21 +0300

    fix indentation

--- test/sort_test.rb
+++ test/sort_test.rb
@@ -8,34 +8,34 @@ class TestShellSort < Test::Unit::TestCase
 
   def setup
     @algo = [:shell_sort, :bubble_sort, :selection_sort]
-  end 
-  
-	def test_empty_array
+  end
+
+  def test_empty_array
     @algo.each do |algo|
       assert_equal(self.send(algo, []), [])
-    end 
-	end 
+    end
+  end
 
-	def test_unordered_array
-		array = [-1,100,0,4,5,0,0]
+  def test_unordered_array
+    array = [-1,100,0,4,5,0,0]
     @algo.each do |algo|
       assert_equal(self.send(algo,array), [-1,0,0,0,4,5,100])
-    end 
-	end 
+    end
+  end
 
-	def test_with_native_sort_function
-		10.times do
-			array = Array(0..1000).shuffle 
+  def test_with_native_sort_function
+    10.times do
+      array = Array(0..1000).shuffle
       @algo.each do |algo|
         assert_equal(self.send(algo,array.dup), array.sort)
       end
-		end 
-	end 
+    end
+  end
 
-	def test_with_real_numbers
-		array = [0,0.1,BigDecimal('1.2'), 1/2r, 1, -1]
-		@algo.each do |algo|
+  def test_with_real_numbers
+    array = [0,0.1,BigDecimal('1.2'), 1/2r, 1, -1]
+    @algo.each do |algo|
       assert_equal(self.send(algo,array), [-1,0,0.1, 1/2r, 1, BigDecimal('1.2')])
-    end 
-	end 
+    end
+  end
 end 
